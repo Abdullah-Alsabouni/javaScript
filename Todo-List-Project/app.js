5667172af2d9d9cadfa61491f8cc5d3bc49c5839
Abdullah Alsabouni <98663047+abdullah012345678910@users.noreply.github.com>
2023-11-14 14:38:43 +0300

    Update app.js

--- Todo-List-Project/app.js
+++ Todo-List-Project/app.js
@@ -2,18 +2,36 @@
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo");
 const todoList = document.querySelector(".list-group");
-const firstCardBody = document.querySelector(".card-body");
-const secondCardBody = document.querySelector(".card-body")[1];
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
 const filter = document.querySelector("#filter");
 const clearButton = document.querySelector("clear-todos");
 
 
-eventListener();
+eventListeners();
 
-function eventListener() { 
+function eventListeners() { 
     form.addEventListener("submit", addTodo);
+    document.addEventListener("DOMContentLoaded", loadAllTodosToUI); // Sayfa yenillendiğinde eklenen ve yapılan değişiklikler silinmez
+    secondCardBody.addEventListener("click", deleteTodo); 
 }
+/////////////////////////////
+function deleteTodo(e) {
+    if (e.target.className === "fa fa-remove"){ //"fa fa-remove" sınıfın olan "X" simgesine tıklandığında silme işlemi olsun
+        e.target.parentElement.parentElement.remove(); // "fa fa-remove" parent ==> <a href = "#" class ="delete-item"> parent ==> <li class="list-group-item d-flex justify-content-between"> ==> Todo'yu sil.
+        showAlert("success", "Todo başariyla silindi");
+    }
+}
+////////////////////////////
+ function loadAllTodosToUI() { // Todos'ları arayüze'de görüntüle
+     let todos = getTodosFromStorage();
 
+     todos.forEach(function(todo){
+         addTodoUI(todo);
+
+     })
+ }
+//////////////////////////
 
 function addTodo(e) {
     const newTodo = todoInput.value.trim(); // trim metodu Inputtaki girilen string'in gereksiz boşluklarını kaldırır
@@ -26,14 +44,55 @@ function addTodo(e) {
     }
     else {
         addTodoUI(newTodo); // Arayüze todo ekle
-        showAlert("success", "Todo başarıyla eklendi...");
+        addTodoStorage(newTodo); // Depolamak
+        showAlert("success", "Todo başarıyla eklendi..."); // Uyarı
     }
+
     
     
 
     e.preventDefault();
 }
 
+// Storage'den Todoları alma fonksiyonu
+
+function getTodosFromStorage(){ 
+    let todos;
+
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+}
+
+// Girilen todo'ları Depolama fonksiyonu
+
+function addTodoStorage(newTodo) {
+    let todos;
+
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+}
+/////////////////////////////
+
+function addTodoStorage(newTodo) {
+    let todos = getTodosFromStorage(); // storage'dan aldıktan sonra...
+
+    todos.push(newTodo); // Application kısmına ekleme işlemi...
+
+    localStorage.setItem("todos",JSON.stringify(todos)); // Application kısmına ekleme işlemi...
+}
+
+
+/////////////////////////////
 function showAlert(type, message) { // if koşulunda Boş bir todo gönderildiğinde Uyarı göster fonksiyonu
     const alert = document.createElement("div");
 
